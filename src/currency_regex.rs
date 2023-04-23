2a0890cee9131434d66319a061e0872ee3fdb58a
adel-ak <adelkerow@gmail.com>
2023-04-23 14:20:27 +0300

    checking is aho_corasick is any better then fancy regex

--- src/currency_regex.rs
+++ src/currency_regex.rs
@@ -1,30 +1,45 @@
-use fancy_regex::Regex;
-use lazy_static::lazy_static;
+use aho_corasick::AhoCorasick;
 
 fn format_dollar_group(
     dollar: String,
     separator: String,
 ) -> String {
-    lazy_static! {
-        static ref GROUP_REGEX: Regex = Regex::new(r"(\d)(?=(\d{3})+\b)").unwrap();
+    let mut formatted_integer = String::new();
+
+    for (i, c) in dollar.chars().rev().enumerate() {
+        formatted_integer.push(c);
+
+        if (i + 1) % 3 == 0 && i != dollar.len() - 1 {
+            formatted_integer.push_str(separator.as_str());
+        }
     }
 
-    let replace = format!("$1{separator}");
-
-    GROUP_REGEX.replace_all(&dollar, replace).to_string()
+    formatted_integer.chars().rev().collect()
 }
 
 fn format_dollar_vedic(
     dollar: String,
     separator: String,
 ) -> String {
-    lazy_static! {
-        static ref VEDIC_REGEX: Regex = Regex::new(r"(\d)(?=(\d\d)+\d\b)").unwrap();
+    let mut formatted_integer = String::new();
+
+    let mut index: usize;
+
+    let mut rem: usize;
+
+    for (i, c) in dollar.chars().rev().enumerate() {
+        formatted_integer.push(c);
+
+        rem = if formatted_integer.len() < 3 { 3 } else { 2 };
+
+        index = i + if formatted_integer.len() < 3 { 1 } else { 0 };
+
+        if index % rem == 0 && i != dollar.len() - 1 {
+            formatted_integer.push_str(separator.as_str());
+        }
     }
 
-    let replace = format!("$1{separator}");
-
-    VEDIC_REGEX.replace_all(&dollar, replace).to_string()
+    formatted_integer.chars().rev().collect()
 }
 
 pub fn format_dollar(
@@ -41,34 +56,69 @@ pub fn format_dollar(
 
 pub fn replace_any_non_numeric_values(
     decimal: String,
-    value_allow_negative: String,
+    value: String,
 ) -> String {
-    lazy_static! {
-        static ref REGEX: fn(String) -> Regex =
-            |decimal: String| Regex::new(&format!(r"[^-\d{decimal}]")).unwrap();
+    let patterns = &[
+        "0",
+        "1",
+        "2",
+        "3",
+        "4",
+        "5",
+        "6",
+        "7",
+        "8",
+        "9",
+        "-",
+        decimal.as_str(),
+    ];
+
+    let ac = AhoCorasick::new(patterns).unwrap();
+
+    let mut new_value: String = String::from("");
+
+    for mat in ac.find_iter(&value) {
+        new_value.push(value.as_bytes()[mat.start()] as char);
     }
 
-    REGEX(decimal)
-        .replace_all(&value_allow_negative, "")
-        .to_string()
+    new_value
 }
 
 pub fn allow_negative_values(value: &str) -> String {
-    lazy_static! {
-        static ref REGEX: Regex = Regex::new(r"\((.*)\)").unwrap();
+    let patterns = &["(", ")"];
+
+    let ac = AhoCorasick::new(patterns).unwrap();
+
+    let mut new_value: String = value.to_string();
+
+    let mut start: usize;
+
+    let mut end: usize;
+
+    for (i, mat) in ac.find_iter(value).enumerate() {
+        start = mat.start() - i;
+
+        end = mat.end() - i;
+
+        new_value.replace_range(start..end, "");
     }
 
-    REGEX.replace(value, "-$1").to_string()
+    new_value
 }
 
 pub fn convert_any_decimal_values(
     decimal: String,
-    numeric_values: String,
+    value: String,
 ) -> String {
-    lazy_static! {
-        static ref REGEX: fn(String) -> Regex =
-            |decimal: String| Regex::new(&("\\".to_string() + &decimal)).unwrap();
+    let patterns = &[decimal.as_str()];
+
+    let ac = AhoCorasick::new(patterns).unwrap();
+
+    let mut new_value: String = value.clone();
+
+    for mat in ac.find_iter(&value) {
+        new_value.replace_range(mat.span().range(), ".");
     }
 
-    REGEX(decimal).replace_all(&numeric_values, ".").to_string()
+    new_value
 }
